f42b422e0114dc1d77afeb56138781e2b8150e5b
Anton Khrapov <antonkhrapov@yandex-team.ru>
2024-02-14 18:46:53 +0700

    Initial Commit

---
+++ Task5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task5
+//
+//  Created by Anton Khrapov on 14.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
